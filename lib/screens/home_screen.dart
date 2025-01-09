9c290c999899db4aa595ee21bd303077b271e734
Wesley00s <wesley300rodrigues@gmail.com>
2025-01-09 18:26:03 -0300

    feat: create home screen

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,139 @@
+import 'package:flutter/material.dart';
+import 'package:imc/res/app_color.dart';
+import 'package:imc/screens/widget/gender_dropdown.dart';
+import 'package:imc/screens/widget/imc_table.dart';
+import 'package:imc/screens/widget/styled_button.dart';
+
+import '../model/Result.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final FocusNode focusNode1 = FocusNode();
+  final FocusNode focusNode2 = FocusNode();
+
+  String? selectedGender;
+  String? weight;
+  String? height;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: AppColor.primaryColor,
+        foregroundColor: Colors.white,
+        title: const Row(
+          children: [
+            Text(
+              'Calculadora IMC',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              const Text(
+                'Descubra seu índice de Massa Corporal',
+                style: TextStyle(fontSize: 18),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              TextField(
+                focusNode: focusNode1,
+                onSubmitted: (_) {
+                  FocusScope.of(context).requestFocus(focusNode2);
+                },
+                keyboardType:
+                    const TextInputType.numberWithOptions(decimal: true),
+                decoration: InputDecoration(
+                  labelText: 'Peso (Kg)',
+                  hintText: '0.0',
+                  hintStyle:
+                      const TextStyle(color: Colors.black26, fontSize: 16),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+                onChanged: (text) {
+                  setState(
+                    () {
+                      weight = text;
+                    },
+                  );
+                },
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextField(
+                focusNode: focusNode2,
+                onSubmitted: (_) {
+                  FocusScope.of(context).unfocus();
+                },
+                keyboardType:
+                    const TextInputType.numberWithOptions(decimal: true),
+                decoration: InputDecoration(
+                  labelText: 'Altura (cm)',
+                  hintText: '0.0',
+                  hintStyle:
+                      const TextStyle(color: Colors.black26, fontSize: 16),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+                onChanged: (text) {
+                  setState(
+                    () {
+                      height = text;
+                    },
+                  );
+                },
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              GenderDropdown(
+                selectedGender: selectedGender,
+                onChanged: (value) {
+                  setState(() {
+                    selectedGender = value;
+                  });
+                },
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              StyledButton(
+                onPressed: () {
+                  Navigator.pushNamed(
+                    context,
+                    '/result_screen',
+                    arguments: {
+                      'result':
+                          Result.calcResult(weight!, height!, selectedGender!)
+                    },
+                  );
+                },
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              const IMCTable(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
