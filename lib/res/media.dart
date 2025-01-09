d0cac72e36ce8e27a326ce6209f9c912a70e7f65
Wesley00s <wesley300rodrigues@gmail.com>
2025-01-09 18:28:14 -0300

    feat: create global media config

---
+++ lib/res/media.dart
@@ -0,0 +1,17 @@
+class AppMedia {
+  static const _baseImage = 'assets/images';
+
+  static const underweightMale = '$_baseImage/underweight_male.png';
+  static const normalMale = '$_baseImage/normal_male.png';
+  static const overweightMale = '$_baseImage/overweight_male.png';
+  static const obeseMale1 = '$_baseImage/obese_male_1.png';
+  static const obeseMale2 = '$_baseImage/obese_male_2.png';
+  static const obeseMale3 = '$_baseImage/obese_male_3.png';
+
+  static const underweightFemale = '$_baseImage/underweight_female.png';
+  static const normalFemale = '$_baseImage/normal_female.png';
+  static const overweightFemale = '$_baseImage/overweight_female.png';
+  static const obeseFemale1 = '$_baseImage/obese_female_1.png';
+  static const obeseFemale2 = '$_baseImage/obese_female_2.png';
+  static const obeseFemale3 = '$_baseImage/obese_female_3.png';
+}
