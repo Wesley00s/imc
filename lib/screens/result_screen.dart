67a439a1d627195025459a0759a373914ab3635a
Wesley00s <wesley300rodrigues@gmail.com>
2025-01-12 23:26:58 -0300

    update: reformat result value and standard font sizes

--- lib/screens/result_screen.dart
+++ lib/screens/result_screen.dart
@@ -26,113 +26,109 @@ class _ResultScreenState extends State<ResultScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        centerTitle: true,
-        backgroundColor: AppColor.primaryColor,
-        foregroundColor: Colors.white,
-        title : const Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text(
-              'Calculadora IMC',
-              style : TextStyle(fontWeight: FontWeight.bold)
-            ),
-          ],
-        )
-      ),
+          centerTitle: true,
+          backgroundColor: AppColor.primaryColor,
+          foregroundColor: Colors.white,
+          title: const Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('Calculadora IMC',
+                  style: TextStyle(fontWeight: FontWeight.bold)),
+            ],
+          )),
       body: SingleChildScrollView(
-        child :
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  children: [
-                    const SizedBox(height: 30),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              const SizedBox(height: 30),
+              Text('Seu índice de massa corporal é: ${result.value.toStringAsFixed(2)}',
+                  style: const TextStyle(
+                    fontWeight: FontWeight.normal,
+                    fontSize: 22,
+                  ),
+                  textAlign: TextAlign.center,
+              ),
 
-                    const Text('Seu índice de massa corporal é : ',
-                    style: TextStyle(fontWeight: FontWeight.normal, fontSize: 22),
-                    textAlign: TextAlign.center),
+              const SizedBox(height: 20),
 
-                    const SizedBox(height: 10),
-
-                    Text(result.value.toStringAsFixed(2), style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 46)),
-
-                    const SizedBox(height: 10),
-
-                    Text(result.imcResponse, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 40,
-                    color: AppColor.primaryColor)),
-
-                    const SizedBox(height: 10),
-
-                    Container(
-                      width: 200,
-                      height: 200,
-                      decoration: BoxDecoration(image: result.decorationImage)
-
-                    ),
-
-                    const SizedBox(height: 10),
-
-                    Container(
-                      width: 350,
-                      height: 270,
-                      decoration: BoxDecoration(
-                        color: AppColor.primaryColor,
-                        borderRadius: BorderRadius.circular(15),
-                      ),
-                      child: Column(
-                        children: [
-                          Text(
-                            'ATENÇÃO',
-                            style: TextStyle(
-                              fontSize: 29,
-                              fontWeight: FontWeight.bold,
-                              color: AppColor.tertiaryColor,
-                            ),
-                            textAlign: TextAlign.center,
-                          ),
-
-                          const Padding(
-                            padding : EdgeInsets.only(bottom: 10),
-                            child:
-                            const Text(
-                              'Existe uma diferença entre massa corporal e gordura corporal.',
-                              style: TextStyle(
-                                fontSize: 19,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.white,
-                              ),
-                              textAlign: TextAlign.center,
-                            ),
-                          ),
-                          const Padding(
-                            padding: EdgeInsets.only(bottom: 10),
-                            child: Text(
-                              '* Massa corporal é o peso total do corpo, que inclui músculos, ossos, órgãos e gordura.',
-                              style: TextStyle(
-                                fontSize: 17,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.white,
-                              ),
-                              textAlign: TextAlign.center,
-                            ),
-                          ),
-                          const Padding(
-                            padding: EdgeInsets.only(bottom: 10),
-                            child: Text(
-                              '* Gordura corporal refere-se à quantidade de gordura no corpo, que em excesso pode prejudicar a saúde.',
-                              style: TextStyle(
-                                fontSize: 17,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.white,
-                              ),
-                              textAlign: TextAlign.center,
-                            ),
-                          ),
-                        ],
-                      ),
+              Text(
+                result.imcResponse,
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 22,
+                    color: AppColor.primaryColor,
                 ),
-               ],
-             ),
+              ),
+              const SizedBox(height: 20),
+              Container(
+                height: 250,
+                width: 250,
+                decoration: BoxDecoration(image: result.decorationImage),
+              ),
+              const SizedBox(height: 20),
+              Container(
+                decoration: BoxDecoration(
+                  color: AppColor.primaryColor,
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'ATENÇÃO:',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: AppColor.tertiaryColor,
+                        ),
+                        textAlign: TextAlign.start,
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.only(bottom: 10, top: 10),
+                        child: Text(
+                          'Existe uma diferença entre massa corporal e gordura corporal.',
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                          textAlign: TextAlign.start,
+                        ),
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.only(bottom: 10),
+                        child: Text(
+                          '· Massa corporal é o peso total do corpo, que inclui músculos, ossos, órgãos e gordura.',
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.normal,
+                            color: Colors.white,
+                          ),
+                          textAlign: TextAlign.start,
+                        ),
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.only(bottom: 10),
+                        child: Text(
+                          '· Gordura corporal refere-se à quantidade de gordura no corpo, que em excesso pode prejudicar a saúde.',
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.normal,
+                            color: Colors.white,
+                          ),
+                          textAlign: TextAlign.start,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
           ),
+        ),
       ),
     );
   }
