47bea6642175579f845b898b07199f3daa10ed80
Wesley00s <wesley300rodrigues@gmail.com>
2025-01-12 23:31:46 -0300

    update: remove sized box on button widgets

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -138,7 +138,7 @@ class _HomeScreenState extends State<HomeScreen> {
                         backgroundColor: AppColor.primaryColor,
                       ),
                     );
-                  },
+                  }
                 },
                 text: 'Calcular',
               ),
